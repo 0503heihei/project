3b46a8963874e3d4cb8c09b7951abafa934b57a2
mcy <mcyelm@163.com>
2016-09-20 15:12:26 +0800

    Merge branch 'Ori-wzw' of https://github.com/0503heihei/project into Ori-wzw
    # Conflicts:
    #	README.md

---
+++ caozuolan.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        .wrap{
+            width:1200px;
+            height:610px;
+            /*border: 1px solid black;*/
+            margin: 0 auto;
+            margin-top: 67px;
+            overflow: hidden;
+
+        }
+        .wrap-left{
+            width:300px;
+            height:600px;
+            border: 1px solid black;
+            float: left;
+        }
+        .wrap-right{
+            width:870px;
+            height:600px;
+            border: 1px solid black;
+            float: right;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrap">
+        <div class="wrap-left"></div>
+        <div class="wrap-right"></div>
+    </div>
+</body>
+</html>
